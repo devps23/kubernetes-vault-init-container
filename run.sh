71cd029bce25a772141a0a39df02c04f035702d6
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-01-24 14:54:18 +0530

    vault credentials

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-vault kv get $SECRET_NAME | sed -n -e '/== Data ==/,$ p' | grep -Ev '== Data ==|^Key|^value|^---'| awk '{print export $1, "=", $2}'>/data/secrets.txt
+vault kv get $SECRET_NAME | sed -n -e '/== Data ==/,$ p' | grep -Ev '== Data ==|^Key|^value|^---'| awk '{print "export " $1"="$2}'>/data/secrets.txt
+cat /data/secrets.txt
