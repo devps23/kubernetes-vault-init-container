058d2dd3f6af694d37ee80b88e310f96fe2ecb55
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-03-13 16:16:12 +0530

    * create a jobs in eks cluster * get vault secrets

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
-vault kv get $SECRET_NAME | sed -n -e '/== Data ==/,$ p' | grep -Ev '== Data ==|^Key|^value|^---'| awk '{print "export " $1"="$2}'>/data/secrets.txt
+vault kv get $SECRET_NAME | sed -n -e '/== Data ==/,$ p' | grep -Ev '== Data ==|^Key|^value|^---'| awk '{print "export "$1"=" \""$2"\""}'>/data/secrets.txt
 cat /data/secrets.txt
+
